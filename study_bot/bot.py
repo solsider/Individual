f341b946f4a11fa691b3343198fccc64265ab291
solsider <defremov11@gmail.com>
2026-01-29 11:29:39 +0300

    Update bot.py

--- study_bot/bot.py
+++ study_bot/bot.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
-from app.handlers import routers  # <-- если у тебя именно app/
+from handlers import routers  
 
 async def main():
     while True:
